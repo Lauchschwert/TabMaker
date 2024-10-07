f6d10b10ba528a226d89565bba3b9f6fee6d6aaf
Niclas <98271044+Lauchschwert@users.noreply.github.com>
2024-10-07 20:31:52 +0200

    Main UI mainly finsihed

--- src/UI/Window.java
+++ src/UI/Window.java
@@ -3,8 +3,17 @@ package UI;
 import javax.swing.*;
 
 public class Window extends JFrame {
+    TabPanel tabPanel;
     public Window() {
         super("Tabmaker");
+        
+        tabPanel = new TabPanel();
+        add(tabPanel);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setResizable(false);
         setVisible(true);
     }
 }
